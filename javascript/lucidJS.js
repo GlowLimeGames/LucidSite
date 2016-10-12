8e3c46577aa2d68b5a11fc4f913690bb9f991bcb
Joseph Gillen <joegillen@ymail.com>
2016-10-12 16:18:08 -0400

    Setting up skeleton of lucid site

---
+++ javascript/lucidJS.js
@@ -0,0 +1,7 @@
+document.getElementById("foot_01").innerHTML = "<div>Lucid - A GlowLimeGames Project</div>" + new Date().getFullYear();
+
+document.getElementById("nav_bar_01").innerHTML = "<ul id='menu'>" +
+"<li><a href='main.html'>Home/About</a></li>" +
+"<li><a href='team.html'>Team</a></li>" +
+"<li><a href='gallery.html'>Gallery</a></li>" +
+"</ul>";
